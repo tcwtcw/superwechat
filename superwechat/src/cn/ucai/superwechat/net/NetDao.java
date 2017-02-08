fa04ea24d278ab918e1e29c692c71efcbbe5123f
tcwtcw <675807942@qq.com>
2017-02-08 20:59:04 +0800

    添加用户登录自己的服务器相关方法

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -2,6 +2,8 @@ package cn.ucai.superwechat.net;
 
 import android.content.Context;
 
+import com.hyphenate.easeui.domain.User;
+
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.utils.MD5;
 import cn.ucai.superwechat.utils.OkHttpUtils;
@@ -27,6 +29,16 @@ public class NetDao {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_UNREGISTER)
                 .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void login(Context context, String username, String password,
+                             OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
                 .targetClass(String.class)
                 .execute(listener);
     }
