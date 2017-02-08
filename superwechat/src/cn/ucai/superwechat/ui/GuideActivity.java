2ac13f6618e59db5eeefbbbc55c46b70822f860f
tcwtcw <675807942@qq.com>
2017-02-08 16:31:35 +0800

    添加butterknife的依赖

--- superwechat/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,7 +1,10 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 
 public class GuideActivity extends BaseActivity {
@@ -10,5 +13,16 @@ public class GuideActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                break;
+            case R.id.btn_register:
+                break;
+        }
     }
 }
