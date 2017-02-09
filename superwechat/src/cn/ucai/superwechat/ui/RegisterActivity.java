e3177a618a6f1d4f4a5379925f8dc1820b73c6f0
tcwtcw <675807942@qq.com>
2017-02-09 11:21:05 +0800

    优化登录注册流程

--- superwechat/src/cn/ucai/superwechat/ui/RegisterActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/RegisterActivity.java
@@ -36,6 +36,7 @@ import cn.ucai.superwechat.domain.Result;
 import cn.ucai.superwechat.net.NetDao;
 import cn.ucai.superwechat.net.OnCompleteListener;
 import cn.ucai.superwechat.utils.CommonUtils;
+import cn.ucai.superwechat.utils.MD5;
 import cn.ucai.superwechat.utils.MFGT;
 import cn.ucai.superwechat.utils.ResultUtils;
 
@@ -147,7 +148,7 @@ public class RegisterActivity extends BaseActivity {
             public void run() {
                 try {
                     // call method in SDK
-                    EMClient.getInstance().createAccount(username, pwd);
+                    EMClient.getInstance().createAccount(username, MD5.getMessageDigest(pwd));
                     runOnUiThread(new Runnable() {
                         public void run() {
                             if (!RegisterActivity.this.isFinishing())
