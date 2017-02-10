c4bc5421d704ce3c6dcebdd833b5c2b91003ef95
tcwtcw <675807942@qq.com>
2017-02-10 20:50:45 +0800

    修改退出登录后还可以进入MainActivity的bug

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -1,7 +1,6 @@
 package cn.ucai.superwechat.utils;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
@@ -12,32 +11,36 @@ import cn.ucai.superwechat.ui.RegisterActivity;
 import cn.ucai.superwechat.ui.SettingsActivity;
 import cn.ucai.superwechat.ui.SplashActivity;
 
-
 /**
- * Created by Administrator on 2017/1/10 0010.
+ * Created by clawpo on 2017/1/10.
  */
 
 public class MFGT {
-    public static void finish(Activity activity) {
+    public static void finish(Activity activity){
         activity.finish();
-        activity.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
     }
 
-    public static void startActivity(Activity context, Class<?> clz) {
+    public static void startActivity(Activity context, Class<?> clz){
         context.startActivity(new Intent(context, clz));
-        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
     }
 
-    public static void startActivity(Activity context, Intent intent) {
+    public static void startActivity(Activity context, Intent intent){
         context.startActivity(intent);
-        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+        context.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
     }
 
-    public static void gotoLogin(Activity activity) {
+    public static void gotoLogin(Activity activity){
         startActivity(activity, LoginActivity.class);
     }
 
-    public static void gotoRegister(Activity activity) {
+    public static void gotoLoginCleanTask(Activity activity){
+        startActivity(activity, new Intent(activity,LoginActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    public static void gotoRegister(Activity activity){
         startActivity(activity, RegisterActivity.class);
     }
 
