0d45bd05ef1e953ccc4c24bb9a2ba18935671536
tcwtcw <675807942@qq.com>
2017-02-15 14:36:17 +0800

    添加好友关系到服务器

--- superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ superwechat/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.Tag;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -661,7 +662,7 @@ public class SuperWeChatHelper {
     public class MyContactListener implements EMContactListener {
 
         @Override
-        public void onContactAdded(String username) {
+        public void onContactAdded(final String username) {
             L.e(TAG,"onContactAdded...username="+username);
             // save contact
             Map<String, EaseUser> localUsers = getContactList();
@@ -673,6 +674,30 @@ public class SuperWeChatHelper {
             }
             toAddUsers.put(username, user);
             localUsers.putAll(toAddUsers);
+            NetDao.addContact(appContext, EMClient.getInstance().getCurrentUser(), username,
+                    new OnCompleteListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            L.e(TAG,"onContactAdded...s="+s);
+                            if (s != null) {
+                                Result result = ResultUtils.getResultFromJson(s, User.class);
+                                if (result != null) {
+                                    User user = (User) result.getRetData();
+                                    if (getAppContactList().containsKey(username)) {
+                                        getAppContactList().put(username, user);
+                                        userDao.saveAppContact(user);
+                                        broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
+                                    }
+
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });
 
             broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
         }
